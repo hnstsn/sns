3fca742625314bb9a6ed2565870d5c942cbdf041
WANSEO\hnsts <hnstsn@naver.com>
2024-11-20 22:07:46 +0900

    포스트 - 삭제(왜 빠졌지)

--- src/main/java/com/example/sns/post/controller/PostController.java
+++ src/main/java/com/example/sns/post/controller/PostController.java
@@ -30,7 +30,7 @@ public class PostController {
         return Response.success(PostResponse.fromPost(post));
     }
 
-    @DeleteMapping()
+    @DeleteMapping("/{postId}")
     public Response<Void> delete(@PathVariable Long postId, Authentication authentication) {
         postService.delete(authentication.getName(), postId);
         return Response.success();
